126d986c691cd317b17dea81ed02e9c8d5876488
liammaricato <liammaricato@gmail.com>
2021-09-20 19:54:55 -0300

    :sparkles: Add JWT logic

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,24 +1,26 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Get()
   getLogin(): string {
-    return "This is the home page for you to log in"
+    return "This is the home page for you to log in";
   }
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
   login(@Request() req): any {
-    return req.user;
+    return this.authService.login(req.user);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('protected')
   getHome(): string {
-    return "This is the home page after you're logged in"
+    return "This is the home page after you're logged in";
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
 
 @Module({
@@ -10,6 +11,7 @@ import { LocalStrategy } from './local.strategy';
     secret: 'SECRET', // use environment variables
     signOptions: { expiresIn: '60s' },
   })],
-  providers: [AuthService, LocalStrategy]
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService]
 })
 export class AuthModule {}
